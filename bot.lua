52b8839599df1210c59217b3f7ef414ee8ae11df
clonerteam <jokertabriz@gmail.com>
2016-07-14 12:37:53 +0430

    Update bot.lua
    Tagir yaft

--- bot.lua
+++ bot.lua
@@ -6,9 +6,9 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "185727483:AAF9PZprMDFoR_mJRfKHS3X5Z1HLnM6Ceww" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
+local bot_api_key = "242373637:AAFO0DCIZjOvLwM094uinlZFad6rxZXj6rk" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
 local You = 179242918  --ID ADMIN ایدی خود را اینجا قرار دهید
-local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
+local BASE_URL = "https://api.telegram.org/bot242373637:AAFO0DCIZjOvLwM094uinlZFad6rxZXj6rk"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
 
